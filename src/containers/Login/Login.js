231fdcafec4ec5742f6cbea6ff9739a1ec1783ea
reshma-ve <reshmayashmina@virtualemployee.com>
2018-06-28 14:13:51 +0530

    Commit new changes

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
 import classes from './Login.css';
-import './Login.css';
-import bootstrap from'../../assets/css/bootstrap.min.css';
-import '../../assets/css/font-awesome.min.css';
-import '../../assets/css/metisMenu.min.css';
-import '../../assets/css/sb-admin-2.css';
-import sbadmin from '../../assets/css/sb-admin-2.css';
-
+// import bootstrap from'../../assets/css/bootstrap.min.css';
+// import '../../assets/css/font-awesome.min.css';
+// import '../../assets/css/metisMenu.min.css';
+// import '../../assets/css/sb-admin-2.css';
+import Button from 'react-bootstrap/lib/Button';
 class Login extends Component {
     render () {
         return (
            <div>
-                <h1 className={classes.check}>checkkkk</h1>
-                ojhkhjbj<span className={classes.testing}>checkkkk</span><br></br>
-                
-                <div className="testing">
-                    ojhkhjbj<span className={classes.testing}>checkkkk</span><br></br>
-                    ojhkhjbj<span className="testing">checkkkk</span>
-                </div>
-                <div className={bootstrap.container}>
-                
+                {/* <div className={bootstrap.container}> */}
+                <div className="container">
                 <div className="row">
                <div className="col-md-4 col-md-offset-4">
                 <div className="login-panel panel panel-default">
@@ -34,7 +25,7 @@ class Login extends Component {
                     </div>
                     <div className="form-group">
                      <input className="form-control" placeholder="Password" name="password" type="password" value="" />
-                    </div>
+                         </div>
                     <div className="checkbox">
                     <label>
                     <input name="remember" type="checkbox" value="Remember Me" />Remember Me
